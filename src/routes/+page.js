a7ed8afa09222234c5999cd27620b9748e13ab35
pstuiver <pstuiverx@gmail.com>
2023-07-30 09:51:28 +0200

    fix typos and formatting

--- src/routes/+page.js
+++ src/routes/+page.js
@@ -75,7 +75,7 @@ export async function load() {
 			<path fill="#FFFFFF" d="M16 28c-.999-1.998-3.657-2-4-2c-2 0-5 2-8 2c-1 0-2-.896-2-2V8c0-1.104 1-2 2-2c3.255 0 6-2 8-2c3 0 4 1.896 4 3v19z" />
 			<path fill="#FFFFFF" d="M16 28c.999-1.998 3.657-2 4-2c2 0 5 2 8 2c1 0 2-.896 2-2V8c0-1.104-1-2-2-2c-3.256 0-6-2-8-2c-3 0-4 1.896-4 3v19z" /></svg>`,
 			bodyHTML: `Social stories are personalised tools to help children understand social situations and events`,
-			link: `services-social stories`,
+			link: `services-social-stories`,
 			buttonHTML: `Read more <span class="sr-only">about Social Stories</span>...`
 		},
 		{
