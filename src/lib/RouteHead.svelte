084e9f736085f2e7c0b946f1c1aed4e63ba7305b
pstuiver <pstuiverx@gmail.com>
2023-08-01 12:10:10 +0200

    try history.go(-1) instead of history.back()

--- src/lib/RouteHead.svelte
+++ src/lib/RouteHead.svelte
@@ -7,7 +7,7 @@
 	<nav class="w-full">
 		<div class="w-full flex items-center justify-between">
 			<div class="flex items-center">
-				<button on:click={() => window.history.back()}>
+				<button on:click={() => window.history.go(-1)}>
 					<svg viewBox="0 0 48 48" class="views-head-icon">
 						<title>Back arrow icon</title>
 						<path d="M13,24 23,13 23,20 34,20 34,28 23,28 23,35Z" />
--- src/routes/about-ot/+page.svelte
+++ src/routes/about-ot/+page.svelte
@@ -9,26 +9,7 @@
 </svelte:head>
 
 <div class="views-container">
-	<!-- <RouteHead {headerColor} {headerTitle} /> -->
-
-	<header class="views-head {headerColor}">
-		<nav class="w-full">
-			<div class="w-full flex items-center justify-between">
-				<div class="flex items-center">
-					<button on:click={() => window.history.back()}>
-						<svg viewBox="0 0 48 48" class="views-head-icon">
-							<title>Back arrow icon</title>
-							<path d="M13,24 23,13 23,20 34,20 34,28 23,28 23,35Z" />
-						</svg>
-					</button>
-					<span class="views-head-back" aria-hidden="true">back</span>
-				</div>
-
-				<h1 class="views-head-text">{headerTitle}</h1>
-				<div class="views-head-dummy" aria-hidden="true">&nbsp;</div>
-			</div>
-		</nav>
-	</header>
+	<RouteHead {headerColor} {headerTitle} />
 
 	<main class="views-content gray-on-blue">
 		<p>
