e1bf6bf1154b4d0d23bd03c9834c469d11d394c0
pstuiver <pstuiverx@gmail.com>
2023-08-05 23:08:01 +0200

    Revert "move postcss files to src/"
    This reverts commit dbbd936867a127a3cf6c43ac910396e68fc21b65.

--- src/routes/(home)/+layout.svelte
+++ src/routes/(home)/+layout.svelte
@@ -1,5 +1,9 @@
 <script>
-	import '../../homepage.postcss';
+	// import { onMount } from 'svelte';
+	import './homepage.postcss';
+	// onMount(() => {
+	// 	window.history.scrollRestoration = 'auto';
+	// });
 </script>
 
 <slot />
