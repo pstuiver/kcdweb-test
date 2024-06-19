0054101da08ccb774b0c442a2e522e5905228811
pstuiver <pstuiverx@gmail.com>
2024-06-19 11:39:02 +0200

    add sveltekit-view-transitions

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,9 @@
 <script>
-	import '../tailwind.postcss';
+// Start of Recently Added
+import { setupViewTransition } from "sveltekit-view-transition";
+setupViewTransition();
+// End of Recently Added
+import "../tailwind.postcss";
 </script>
 
 <slot />
