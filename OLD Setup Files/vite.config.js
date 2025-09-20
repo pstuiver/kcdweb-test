440d9422d18292e6f563d4e52f57e5098476dce6
pstuiver <pstuiverx@gmail.com>
2025-09-20 19:16:43 +0200

    sort out prettier settings and do npm run format

--- OLD Setup Files/vite.config.js
+++ OLD Setup Files/vite.config.js
@@ -1,7 +1,6 @@
 import { sveltekit } from "@sveltejs/kit/vite";
 import { defineConfig } from "vite";
-import devtoolsJson from "vite-plugin-devtools-json";
 
 export default defineConfig({
-	plugins: [sveltekit(), devtoolsJson()]
+  plugins: [sveltekit()],
 });
--- svelte.config.js
+++ svelte.config.js
@@ -1,21 +1,13 @@
 import adapter from "@sveltejs/adapter-static";
-import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
-
-// const dev = process.argv.includes('dev');
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  preprocess: vitePreprocess(),
-
-  kit: {
-    adapter: adapter({ pages: "build", assets: "build" }),
-    paths: {
-      base: process.env.NODE_ENV === "production" ? "/kcdweb-test" : "",
-    },
-    output: {
-      bundleStrategy: "single",
-    },
-  },
+	kit: {
+		adapter: adapter({ pages: "build", assets: "build" }),
+		paths: {
+			base: process.env.NODE_ENV === "production" ? "/kcdweb-test" : ""
+		}
+	}
 };
 
 export default config;
